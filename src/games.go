f2a30d1625c19d858d8e01f37c8cc0456c34d62b
mostafa qari <mo30qari@gmail.com>
2020-12-25 11:18:40 +0330

    some primary changes

--- src/games.go
+++ src/games.go
@@ -2,41 +2,66 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 	"net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-	
-	fmt.Println("helloWorld")
+var db *gorm.DB
+var err error
 
+type Game struct {
+	gorm.Model
+	Name string
+	Genre string
+	Price int
 }
 
-func createGame(w http.ResponseWriter, r *http.Request){
+func initialMigration(){
 	
+	db, err = gorm.Open("mysql", "root:@/dds_db?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Failed to Open Database!")
+	}
+	defer db.Close()
+	
+	db.AutoMigrate(&Game{})
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	
+	fmt.Println("helloWorld")
+	
+}
+
+func createGame(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println(1)
 	fmt.Println("createGame: " + mux.Vars(r)["name"] + "@" + mux.Vars(r)["genre"])
 	
 }
 
-func getAllGames(w http.ResponseWriter, r *http.Request){
+func getAllGames(w http.ResponseWriter, r *http.Request) {
 	
 	fmt.Println("getAllGames")
 	
 }
 
-func getGame(w http.ResponseWriter, r *http.Request){
+func getGame(w http.ResponseWriter, r *http.Request) {
 	
 	fmt.Println("getGame")
 	
 }
 
-func updateGame(w http.ResponseWriter, r *http.Request){
+func updateGame(w http.ResponseWriter, r *http.Request) {
 	
 	fmt.Println("updateGame")
 	
 }
 
-func deleteGame(w http.ResponseWriter, r *http.Request){
+func deleteGame(w http.ResponseWriter, r *http.Request) {
 	
 	fmt.Println("deleteGame")
 	
--- src/main.go
+++ src/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm"
 	"log"
 	"net/http"
 )
 
-func main(){
-
+func main() {
+	
+	initialMigration()
+	
 	handleRequests()
-
+	
 }
 
-func handleRequests(){
+func handleRequests() {
 	
 	router := mux.NewRouter()
-	router.HandleFunc("/", helloWorld).Methods("GET")
-	router.HandleFunc("/game/{name}/{genre}", createGame).Methods("GET")
+	
+	router.HandleFunc("/", welcome).Methods("GET")
+	router.HandleFunc("/game/{name}/{genre}/{price}", createGame).Methods("GET")
 	router.HandleFunc("/games", getAllGames).Methods("GET")
 	router.HandleFunc("/game/{name}", getGame).Methods("GET")
 	router.HandleFunc("/game/{name}", updateGame).Methods("PUT")
 	router.HandleFunc("/game/{name}", deleteGame).Methods("DELETE")
+	
 	log.Fatal(http.ListenAndServe(":8080", router))
 	
 }
